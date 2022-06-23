53dbb9513f69ba232e7d9fb6cf66e44d40f3980b
marufhosen <maruf15-1591@diu.edu.bd>
2022-06-23 15:09:47 +0600

    first commit

---
+++ src/routes/users/[id].svelte
@@ -0,0 +1,22 @@
+<script context="module">
+  export async function load({ params }) {
+    const id = params.id;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const user = await res.json();
+
+    return {
+      props: {
+        user,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let user;
+</script>
+
+<div class="guide">
+  <h2>{user.name}</h2>
+  <p>{user.email}</p>
+</div>
---
+++ src/routes/users/index.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  export async function load() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    console.log(users);
+    return {
+      props: {
+        users,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let users;
+</script>
+
+<div class="guides">
+  <ul>
+    {#each users as user}
+      <li>
+        <a href={`/users/${user.id}`}>{user.name}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
